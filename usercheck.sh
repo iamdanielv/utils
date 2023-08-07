b68821fccc0c77cfd971972a04202874215ed99c
iamdanielv <1767914+iamdanielv@users.noreply.github.com>
2023-08-07 11:18:25 -0700

    ADD: User check

---
+++ usercheck.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+# Check if we are currently running as daniel
+# if not, try to switch to daniel using sudo
+echo "This is run as $USER"
+
+if ! [ "${USER}" == "daniel" ]
+then
+    echo "tyring to switch to daniel"
+    sudo -H -u daniel bash -c './test.sh'
+else
+    echo "found user ${USER}, exiting"
+    exit;
+fi
