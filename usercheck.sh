878f8c31407f8f8ca17997c8faca8dec0dc8cf4c
iamdanielv <1767914+iamdanielv@users.noreply.github.com>
2025-06-12 16:49:24 -0700

    usercheck
    fix typo
    
    make target user a variable
    
    more verbose output and use color and icons to show success or failure

--- usercheck.sh
+++ usercheck.sh
@@ -1,14 +1,25 @@
 #!/usr/bin/bash
 
-# Check if we are currently running as daniel
-# if not, try to switch to daniel using sudo
+# Define the user to switch to
+TARGET_USER="daniel"
+
+# Check if we are currently running as TARGET_USER
+# if not, try to switch to TARGET_USER using sudo
 echo "This is run as $USER"
 
-if ! [ "${USER}" == "daniel" ]
+if ! [ "${USER}" == "${TARGET_USER}" ]
 then
-    echo "tyring to switch to daniel"
-    sudo -H -u daniel bash -c './usercheck.sh'
+    echo -e "Trying to switch to \033[34m${TARGET_USER}...\033[0m"
+    # Use sudo to switch user and run the script again
+    if sudo -H -u "${TARGET_USER}" bash -c './usercheck.sh'
+    then
+        echo -e "\033[32m✓ Switched to ${TARGET_USER} successfully\033[0m"
+        exit 0
+    else
+        echo -e "\033[31m✗ Failed to switch to ${TARGET_USER}\033[0m"
+        exit 1
+    fi
 else
-    echo "found user ${USER}, exiting"
-    exit;
+    echo -e "\033[32m✓ Found user ${USER}\033[0m, exiting"
+    exit 0
 fi
