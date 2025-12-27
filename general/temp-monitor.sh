ac5fc09320e309fe4ae0bb408c0d0068bf146772
iamdanielv <1767914+iamdanielv@users.noreply.github.com>
2025-12-27 14:16:49 -0700

    refactor: temp-monitor.sh - remove dead code

--- general/temp-monitor.sh
+++ general/temp-monitor.sh
@@ -9,10 +9,7 @@ set -o pipefail
 
 # Colors & Styles
 C_RED=$'\033[31m'
-C_GREEN=$'\033[32m'
 C_YELLOW=$'\033[33m'
-C_BLUE=$'\033[34m'
-C_CYAN=$'\033[36m'
 C_L_RED=$'\033[31;1m'
 C_L_GREEN=$'\033[32m'
 C_L_YELLOW=$'\033[33m'
