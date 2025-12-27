019e5bc8d2355f21513fa68b35ca689bbebd4402
iamdanielv <1767914+iamdanielv@users.noreply.github.com>
2025-12-27 14:14:38 -0700

    refactor: setup-dev-machine.sh - remove dead code

--- setup-dev-machine.sh
+++ setup-dev-machine.sh
@@ -8,8 +8,6 @@ set -o pipefail
 C_RED=$'\033[31m'
 C_GREEN=$'\033[32m'
 C_YELLOW=$'\033[33m'
-C_BLUE=$'\033[34m'
-C_CYAN=$'\033[36m'
 C_L_RED=$'\033[31;1m'
 C_L_GREEN=$'\033[32m'
 C_L_YELLOW=$'\033[33m'
